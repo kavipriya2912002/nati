4754a22d50fa239541c02d834720cf7480743d68
kavipriya2912002 <kavipriya2912002@gmail.com>
2024-03-08 17:44:06 +0530

    header and packages install

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import React from "react";
+import { Link} from 'react-router-dom';
+import { Footer } from "flowbite-react";
+import {BsFacebook,BsInstagram,BsTwitter,BsGithub} from 'react-icons/bs'
 
-export default function Footer() {
+export default function FooterCom() {
   return (
-    <div>
-footer
-    </div>
+    <Footer container className="border border-t-8 border-teal-500">
+        <div className="w-full max-w-7xl mx-auto">
+            <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+                <div className="mt-5">
+                <Link to="/" className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'>
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>HK's</span>
+            Blog
+        </Link>
+
+                </div>
+
+                <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+                    <div>
+                    <Footer.Title title="ABOUT" />
+                    <Footer.LinkGroup col>
+                        <Footer.Link href="https://www.w3schools.com/js/DEFAULT.asp" target='_blank' rel='noopener noreferrer'>
+                           Learn JavaScript
+                        </Footer.Link>
+                        <Footer.Link href="/about" target='_blank' rel='noopener noreferrer'>
+                           HK's Blog
+                        </Footer.Link>
+                    </Footer.LinkGroup>
+
+                    </div>
+
+                    <div>
+                    <Footer.Title title="FOLLOW US" />
+                    <Footer.LinkGroup col>
+                        <Footer.Link href="https://www.github.com/kavipriya2912002" target='_blank' rel='noopener noreferrer'>
+                           Github
+                        </Footer.Link>
+                        <Footer.Link href="#">
+                           Discord
+                        </Footer.Link>
+                    </Footer.LinkGroup>
+
+                    </div>
+
+                    <div>
+                    <Footer.Title title="LEGAL" />
+                    <Footer.LinkGroup col>
+                        <Footer.Link href="#">
+                           Privacy
+                        </Footer.Link>
+                        <Footer.Link href="#">
+                          Terms &amp; Conditions
+                        </Footer.Link>
+                    </Footer.LinkGroup>
+
+                    </div>
+                    
+
+                </div>
+
+            </div>
+
+            <Footer.Divider />
+            <div className="w-full sm:flex sm:items-center sm:justify-between ">
+                <Footer.Copyright href="#" by="HK's blog" year={new Date().getFullYear()}/>
+                <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+                    <Footer.Icon href='#' icon={BsFacebook}/>
+                    <Footer.Icon href='#' icon={BsInstagram}/>
+                    <Footer.Icon href='#' icon={BsTwitter}/>
+                    <Footer.Icon href='https://www.github.com/kavipriya2912002' icon={BsGithub}/>
+                    
+                </div>
+
+            </div>
+        </div>
+    </Footer>
   )
 }
--- client/src/components/ScrollToTop.jsx
+++ client/src/components/ScrollToTop.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
-export default function ScrollToTop() {
-  return (
-    <div>
-      Scrolltotop
-    </div>
-  )
-}
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
+
+export default ScrollToTop;
